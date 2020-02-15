c2e5baced760ba48aa8ad363ede04ebb88b02aa4
Midstackdev <midstackdev@gmail.com>
2020-02-15 21:02:16 +0000

    Book routes

--- server/server.js
+++ server/server.js
@@ -14,6 +14,58 @@ const  { Book } = require('./models/book')
 app.use(bodyParser.json())
 app.use(cookieParser())
 
+app.get('/api/getBook', (req, res) => {
+    let id = req.query.id
+
+    Book.findById(id, (err, doc) => {
+        if(err) return res.status(400).send(err)
+        res.send(doc)
+    })
+})
+
+app.get('/api/books', (req, res) => {
+
+    let skip = parseInt(req.query.skip)
+    let limit = parseInt(req.query.limit)
+    let order = req.query.order
+
+    Book.find().skip(skip).sort({_id:order}).limit(limit).exec((err, doc) => {
+        if(err) return res.status(400).send(err)
+        res.send(doc)
+    })
+})
+
+app.post('/api/book', (req, res) => {
+    const book = new Book(req.body)
+
+    book.save((err, doc) => {
+        if(err) return res.status(400).send(err)
+        res.status(200).json({
+            post: true,
+            bookId: doc._id
+        })
+    })
+})
+
+app.post('/api/book_update', (req, res) => {
+    Book.findByIdAndUpdate(req.body._id, req.body, {new:true}, (err, doc) => {
+        if(err) return res.status(400).send(err)
+        res.status(200).json({
+            success: true,
+            doc
+        })
+    })
+})
+
+app.delete('/api/book_delete', (req, res) => {
+    let id = req.query.id
+
+    Book.findByIdAndRemove(id, (err, doc) => {
+        if(err) return res.status(400).send(err)
+        res.status(200).json(true)
+    })
+})
+
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {
