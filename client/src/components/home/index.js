1a46bcdd4ccbeeb6ab99defd62edc86399518a7e
Midstackdev <midstackdev@gmail.com>
2020-02-21 05:55:54 +0000

    Client home

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import HomeContainer from '../../containers/home'
+
 const Home = () => {
 
     return (
         <div>
-            Home
+            <HomeContainer />
         </div>
     )
 }
