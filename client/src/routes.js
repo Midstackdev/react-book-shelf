96e7017bfdd54d84e533d29b419f4a0e5772e73f
Midstackdev <midstackdev@gmail.com>
2020-02-29 19:41:38 +0000

    Auth client hoc

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,17 +2,21 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from './components/home'
+import User from './components/admin'
 import Login from './containers/admin/login'
 import BookView from './components/books'
 import Layout from './hoc/layout'
+
+import Auth from  './hoc/auth'
 
 const Routes = (props) => {
     return (
         <Layout>
             <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/login" exact component={Login} />
-                <Route path="/books/:id" exact component={BookView} />
+                <Route path="/" exact component={Auth(Home, null)} />
+                <Route path="/login" exact component={Auth(Login, false)} />
+                <Route path="/user" exact component={Auth(User, true)} />
+                <Route path="/books/:id" exact component={Auth(BookView)} />
             </Switch>
         </Layout>
     )
